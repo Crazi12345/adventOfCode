4ff413ecf9a743de8e95e8ae2af8375e1be3cab3
Crazi12345 <patrickbjerre@outlook.dk>
2023-12-01 13:03:39 -0500

    cleaned it up

--- 2023/C++/Day1/part_two.cpp
+++ 2023/C++/Day1/part_two.cpp
@@ -1,34 +1,7 @@
 #include <iostream>
 #include <fstream>
-#include <iterator>
 #include <ostream>
 #include <string>
-
-std::string custom_substring(int start, int end, std::string full_string){
-    if(end>full_string.size()){
-            return "";
-        }
-    std::string result; 
-    int j = 0;
-    for(int i = start; i < end; i++){
-        result+= full_string[i]; 
-        j++;
-    }
-    return result;
-}
-std::string custom_replace(int start, int end, std::string full_string, char newChar){
-    if(end>full_string.size()){
-        std::cout << "TOO BIG" << std::endl;
-        return full_string;
-    }
-   std::string result;
-    result = custom_substring(0,start,full_string);
-
-    result+=newChar;
-    result+= custom_substring(end,full_string.size(),full_string);
-    return result;
-
-}
 int string_to_int(std::string number){
     if(number == "one"){
         return 1;
@@ -92,17 +65,9 @@ int main(){
                         last = mystring[i];
                     }
                    }
- char first = '0', last = '0';
-            for (char c : mystring) {
-                if (isdigit(c)) {
-                    if (first == '0') first = c;
-                    last = c;
-                }
-            }
 
             std::string numberStr = {first, last};
             int number = std::stoi(numberStr);  
-            std::cout << first << last << " " << mystring << "  " << (number)<<std::endl;
            sum +=(number);
         }
     }
