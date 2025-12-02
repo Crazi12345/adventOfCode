bde6c485aacdebe69615cc68169ecb55a1c15b76
Patrick Bjerre <patrickbjerre@outlook.dk>
2025-12-02 13:19:09 +0100

    finished d2p2

--- 2025/Go/puzzles/day2Puz2.go
+++ 2025/Go/puzzles/day2Puz2.go
@@ -10,15 +10,15 @@ import (
 
 func Day2Puz2() {
 	defer duration(track("Day2Puz2"))
-	file, err := os.OpenFile("TestDay2.txt", os.O_RDWR, 0644)
+	file, err := os.OpenFile("InputDay2.txt", os.O_RDWR, 0644)
 	check(err, "could not open file")
 	defer file.Close()
+	log.SetOutput(os.Stdout)
 	reader := bufio.NewReader(file)
 	data, _, err := reader.ReadLine()
 	dataString := string(data)
 	check(err, "could not read line")
 	ranges := strings.Split(dataString, ",")
-
 	invalidSum := 0
 	for i := 0; i < len(ranges); i++ {
 		values := strings.Split(ranges[i], "-")
@@ -26,15 +26,17 @@ func Day2Puz2() {
 		check(err, "could not convert1")
 		maxval, err := strconv.Atoi(values[1])
 		check(err, "could not convert2")
-		for j := minval; j < maxval; j++ {
+		for j := minval; j <= maxval; j++ {
 			id := strconv.Itoa(j)
-			for k := 2; k < len(id)/2; k++ {
+			for k := 2; k <= len(id); k++ {
 				step := len(id) / k
+				if len(id)%k > 0 {
+					continue
+				}
 				lastSeen := id[:step]
 				isEqual := true
 				for m := step; m < len(id); m = m + step {
 
-					log.Print(id[m:m+step] +" "+ lastSeen+ "   " + id)
 					if id[m:m+step] != lastSeen {
 						isEqual = false
 						break
@@ -42,18 +44,12 @@ func Day2Puz2() {
 					lastSeen = id[m : m+step]
 				}
 				if isEqual == true {
-					log.Println(lastSeen+ " hit:"+ id)
+					// log.Println(lastSeen + " hit:" + id)
 					invalidSum += j
 					break
 				}
 			}
-
 		}
-
-	}
-
-	if invalidSum == 4174379265 {
-		log.Println("YOU GOT IT CORRECT")
 	}
 	log.Println(invalidSum)
 }
